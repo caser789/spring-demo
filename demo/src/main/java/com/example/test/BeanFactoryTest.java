a36811df69455aff496c0a7cf573fcaa7043a4ae
jiao.xue1 <jiao.xue1@bytedance.com>
2024-03-29 15:18:30 +0800

    bean factory
        1. load xml into bean factory
        2. get bean by bean factory

---
+++ demo/src/main/java/com/example/test/BeanFactoryTest.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+public class BeanFactoryTest {
+    public static void main(String[] args) {
+        // 1. create bean factory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 2. load xml file
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        // 3. factory bind xml
+        reader.loadBeanDefinitions("beans.xml");
+        // 4. get bean by id
+        Object userService = beanFactory.getBean("userService");
+        System.out.println(userService);
+    }
+}
