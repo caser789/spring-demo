39bb5716d37c51ed93556446815fc9304c287746
jiao.xue1 <jiao.xue1@bytedance.com>
2024-03-29 18:49:06 +0800

    实例工厂

--- demo/src/main/java/com/example/test/DruidTest.java
+++ demo/src/main/java/com/example/test/DruidTest.java
@@ -6,12 +6,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class DruidTest {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         Object dataSource = applicationContext.getBean("connection");
         System.out.println(dataSource);
+
+        // SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        // Date parse = simpleDateFormat.parse("2023-10-11");
+        Object date = applicationContext.getBean("date");
+        System.out.println(date);
     }
 }
