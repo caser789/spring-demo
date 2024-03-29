f331cfeaedec651976128e3874296a6b2c972796
jiao.xue1 <jiao.xue1@bytedance.com>
2024-03-29 19:06:23 +0800

    mybatis session xml

--- demo/src/main/java/com/example/test/DruidTest.java
+++ demo/src/main/java/com/example/test/DruidTest.java
@@ -2,8 +2,14 @@ package com.example.test;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.example.service.impl.UserServiceImpl;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.text.ParseException;
@@ -12,7 +18,7 @@ import java.util.Date;
 import java.util.List;
 
 public class DruidTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         Object dataSource = applicationContext.getBean("connection");
         System.out.println(dataSource);
@@ -21,5 +27,23 @@ public class DruidTest {
         // Date parse = simpleDateFormat.parse("2023-10-11");
         Object date = applicationContext.getBean("date");
         System.out.println(date);
+
+        // mybatis
+        // static factory method
+        // InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        // new
+        // SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // instance factory method
+        // SqlSessionFactory factory = builder.build(stream);
+
+        // SqlSessionFactory factory = applicationContext.getBean("sqlSessionFactory", SqlSessionFactory.class);
+        // System.out.println(factory);
+        // SqlSession session = factory.openSession();
+        // System.out.println(session);
+
+        SqlSession session = applicationContext.getBean("session", SqlSession.class);
+        System.out.println(session);
+        SqlSession session1 = applicationContext.getBean("session", SqlSession.class);
+        System.out.println(session1);
     }
 }
